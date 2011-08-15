c42bb0d034635449e7a64b47f586a825dfc629f8
Jay Feldblum <y_feldblum@yahoo.com>
2011-08-15 11:42:20 -0400

    Initial commit.

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: bundle exec thin start -p $PORT
+console: bundle exec rails console
+f: bundle exec script/f
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleRails::Application.config.secret_token = '726e5ac13819272110653fab0e93b7ee003db5dc74477df93e7b43f12f1b7aeb615ec8a97a6c41908708413d342f54a32ff3acd23bedc50db8d3b33aef2d7243'
